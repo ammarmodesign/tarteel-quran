2aab9b27dab26cb9e44c8900868ff8a0c55f80c3
ammarmodesign <ammarmodesign@gmail.com>
2024-01-20 10:54:49 +0200

    404 custom

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,6 @@
 const NotFound = () => {
   return (
-    <div className="my-64 text-center">
+    <div className="my-32 md:my-64 text-center">
       <div className="wrapper">
         <h1 className="font-bold text-white text-4xl md:text-8xl">
           404 الصفحة غير موجودة
